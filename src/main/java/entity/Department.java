44bc8a0fa1d709b651e0f5afe2b2e8c7a6c3d96b
germansayhi <120578921+germansayhi@users.noreply.github.com>
2024-07-10 21:18:44 +0700

    Create

--- src/main/java/entity/Department.java
+++ src/main/java/entity/Department.java
@@ -1,2 +1,35 @@
-package entity;public class Department {
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "department")
+public class Department {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", length = 50, unique =true , nullable = false)
+    private String name;
+
+    @Column(name =" create_at",nullable = false, updatable = false )
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime updateAt;
 }
+
+
+
